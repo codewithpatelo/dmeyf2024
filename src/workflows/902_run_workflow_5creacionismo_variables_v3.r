fd2b770e896f70c0ceb918beae59c7e216d65477
Patricio <pj.patriciojulian@gmail.com>
2024-11-19 19:41:35 -0300

    add etapa fehist con valvulas

--- src/workflows/902_run_workflow_5creacionismo_variables_v3.r
+++ src/workflows/902_run_workflow_5creacionismo_variables_v3.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/916_workflow_5creacionismo_normal_v3.r"
+PARAM <- "src/workflows/916_workflow_6creacionismo_normal_v3.r"
 
 envg <- env()
 
